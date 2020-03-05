241a640660454e3e92eadb1e74383d02c15c26a7
splendor <1742300724@qq.com>
2020-03-05 14:46:30 +0800

    封装登陆页面元素&修改元素属性变量名的拼写

--- page/login_page.py
+++ page/login_page.py
@@ -1,8 +1,24 @@
 """
 登录页面
 """
+import page
 from base.base_page import BasePage
 
 
 class LoginPage(BasePage):
-    pass
+    phone=page.phone
+    pwd=page.pwd
+    login_btn=page.login_btn
+    confirm_btn=page.comfirm_btn
+
+    def input_phone(self,phone):
+        self.input_func(self.phone,phone)
+
+    def input_pwd(self,pwd):
+        self.input_func(self.pwd,pwd)
+
+    def click_login_btn(self):
+        self.click_func(self.login_btn)
+
+    def click_confirm_btn(self):
+        self.click_func(self.confirm_btn)
